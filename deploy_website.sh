713401eaefb386f52a5d7cb897411e36e6aa9b43
BiaoWu <biaoliu_2007@163.com>
2016-07-30 16:26:23 +0800

    add deploy_website.sh

---
+++ deploy_website.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -ex
+
+DIR=_site
+
+rm -rf $DIR
+
+# jekyll build
+jekyll build
+
+# Checkout and track the gh-pages branch
+git checkout gh-pages
+
+# Delete everything that isn't _site
+ls | grep -E -v '^_site$' | xargs rm -rf
+
+cp -R $DIR/* .
+
+# Delete our temp folder
+rm -rf $DIR
+
+# Delete our temp folder
+rm -rf .sass-cache
+
+# Delete our temp folder
+rm -rf .jekyll-metadata
+
+# Stage all files in git and create a commit
+git add .
+git add -u
+git commit -m "Website at $(date)"
+
+# Push the new files up to GitHub
+git push origin gh-pages
